0a4d90f9306bac7e36be0b1cfd90dc911b34ca6c
Jefri Manurung <jefri.manurung@gtech.digital>
2022-08-30 13:46:14 +0700

    update icon and title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   target: 'static',
   ssr: false,
   head: {
-    title: 'Wordle ID',
+    title: 'Wordle Indonesia',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
